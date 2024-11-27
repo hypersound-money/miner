8cc1aa7083b82a1da4b1e79cfe5e3dcd2260f2f7
DaWe <willteam35@gmail.com>
2024-11-27 22:23:44 +0100

    replace ethers with viem, remove nonce to avoid losing ETH

--- mine.js
+++ mine.js
@@ -1,48 +1,63 @@
 require('dotenv').config()
-const ethers = require('ethers')
+const { createPublicClient, createWalletClient, http } = require('viem')
+const { privateKeyToAccount } = require('viem/accounts')
+const { blast } = require('viem/chains')
 
-const provider = new ethers.JsonRpcProvider(process.env.RPC)
-const miner = new ethers.Wallet(process.env.PRIVATE_KEY, provider)
+const publicClient = createPublicClient({
+	chain: blast,
+	transport: http(process.env.RPC)
+})
+
+const privateKey = process.env.PRIVATE_KEY.startsWith('0x') ? process.env.PRIVATE_KEY : `0x${process.env.PRIVATE_KEY}`
+const account = privateKeyToAccount(privateKey)
+const walletClient = createWalletClient({
+	chain: blast,
+	transport: http(process.env.RPC),
+	account
+})
+
 const HYPERSOUND_ADDRESS = '0xF8797dB8a9EeD416Ca14e8dFaEde2BF4E1aabFC3'
 const HYEPRSOUND_ABI = require('./abi.json')
-const hypersound = new ethers.Contract(HYPERSOUND_ADDRESS, HYEPRSOUND_ABI, miner)
 
-async function mine (_nonce) {
+async function mine() {
 	try {
-		let tx
-	
-		if (parseInt(process.env.CALLS_PER_MINE_TX) === 1) {
-			tx = await hypersound.connect(miner).mine('0x', { 
-				nonce: _nonce, 
-				gasLimit: parseInt('50000') 
+		let hash
+		const mineAmount = process.env.CALLS_PER_MINE_TX
+		if (mineAmount === '1') { // mine one with mine()
+			hash = await walletClient.writeContract({
+				address: HYPERSOUND_ADDRESS,
+				abi: HYEPRSOUND_ABI,
+				functionName: 'mine',
+				args: ['0x'],
+				account,
+				gas: BigInt(200000)
 			})
-		} else {
-			tx = await hypersound.connect(miner).mineBatch(
-				parseInt(process.env.CALLS_PER_MINE_TX), 
-				'0x', 
-				{ 
-					nonce: _nonce, 
-					gasLimit: parseInt('25000000') 
-				}
-			)
+		} else { // mine multiple with mineBatch()
+			hash = await walletClient.writeContract({
+				address: HYPERSOUND_ADDRESS,
+				abi: HYEPRSOUND_ABI,
+				functionName: 'mineBatch',
+				args: [BigInt(mineAmount), '0x'],
+				account,
+				gas: BigInt(27000000)
+			})
 		}
-		
-		console.log('Mine transaction sent with the nonce', _nonce)
-		
-		tx.wait().then(result => {
-			console.log('Mine transaction confirmed with the nonce', _nonce)
-		})
+		console.log('Mine transaction sent. Hash:', hash)
+
+		const receipt = await publicClient.waitForTransactionReceipt({ hash })
+		console.log('Mine transaction confirmed')
 	} catch (e) {
-		console.error('An error occurred while executing the transaction with the nonce', _nonce, e)
-		console.log(e.code, e.info, e?.data?.info)
+		console.error('An error occurred while executing the transaction', e)
 	}
 }
 
-provider.getTransactionCount(miner.address).then(nonce => {
-	mine(nonce)
+async function start() {
+	mine()
 
 	setInterval(() => {
-		mine(++nonce)
+		mine()
 	}, 60000 / parseInt(process.env.MINE_TX_PER_MINUTE))
-})
+}
+
+start()
 
