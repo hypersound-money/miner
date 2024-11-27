e7d82c0b90af864b25d8adb8abdf96245b9ee598
DaWe <willteam35@gmail.com>
2024-11-27 21:52:14 +0100

    dockerize, .env, readme

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    PRIVATE_KEY: '',
-    CALLS_PER_MINE_TX: 1,
-    MINE_TX_PER_MINUTE: 10,
-    RPC: 'https://rpc.ankr.com/blast',
-    GAS_LIMIT: 19930000
-}
--- mine.js
+++ mine.js
@@ -1,30 +1,40 @@
-const config = require('./config.js');
-const ethers = require('ethers');
-const provider = new ethers.JsonRpcProvider(config.RPC);
-const miner = new ethers.Wallet(config.PRIVATE_KEY, provider);
-const HYPERSOUND_ADDRESS = '0xF8797dB8a9EeD416Ca14e8dFaEde2BF4E1aabFC3';
-const HYEPRSOUND_ABI = require('./abi.json')
-const hypersound = new ethers.Contract(HYPERSOUND_ADDRESS, HYEPRSOUND_ABI, miner);
+require('dotenv').config()
+const ethers = require('ethers')
 
+const provider = new ethers.JsonRpcProvider(process.env.RPC)
+const miner = new ethers.Wallet(process.env.PRIVATE_KEY, provider)
+const HYPERSOUND_ADDRESS = '0xF8797dB8a9EeD416Ca14e8dFaEde2BF4E1aabFC3'
+const HYEPRSOUND_ABI = require('./abi.json')
+const hypersound = new ethers.Contract(HYPERSOUND_ADDRESS, HYEPRSOUND_ABI, miner)
 
 async function mine (_nonce) {
 	try {
 		let tx
 	
-		if (config.CALLS_PER_MINE_TX === 1) {
-			tx = await hypersound.connect(miner).mine('0x', { nonce: _nonce, gasLimit: config.GAS_LIMIT })
+		if (parseInt(process.env.CALLS_PER_MINE_TX) === 1) {
+			tx = await hypersound.connect(miner).mine('0x', { 
+				nonce: _nonce, 
+				gasLimit: parseInt('50000') 
+			})
 		} else {
-			tx = await hypersound.connect(miner).mineBatch(config.CALLS_PER_MINE_TX, '0x', { nonce: _nonce, gasLimit: config.GAS_LIMIT })
+			tx = await hypersound.connect(miner).mineBatch(
+				parseInt(process.env.CALLS_PER_MINE_TX), 
+				'0x', 
+				{ 
+					nonce: _nonce, 
+					gasLimit: parseInt('25000000') 
+				}
+			)
 		}
 		
 		console.log('Mine transaction sent with the nonce', _nonce)
 		
 		tx.wait().then(result => {
 			console.log('Mine transaction confirmed with the nonce', _nonce)
-	    	})
+		})
 	} catch (e) {
 		console.error('An error occurred while executing the transaction with the nonce', _nonce, e)
-        	console.log(e.code, e.info, e?.data?.info)
+		console.log(e.code, e.info, e?.data?.info)
 	}
 }
 
@@ -33,6 +43,6 @@ provider.getTransactionCount(miner.address).then(nonce => {
 
 	setInterval(() => {
 		mine(++nonce)
-	}, 60000 / config.MINE_TX_PER_MINUTE)
+	}, 60000 / parseInt(process.env.MINE_TX_PER_MINUTE))
 })
 
